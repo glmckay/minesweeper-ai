9e580b3890d15152e79178760c5e0f5692a5d88b
glmckay <glmckay@uwaterloo.ca>
2020-10-28 22:36:23 -0400

    betamine can now return something that could be passed to minesweeper.Game.process_move

--- betamine.py
+++ betamine.py
@@ -2,15 +2,21 @@ from agent import agent
 import numpy
 import minesweeper
 
+
+width = 10
+height = 10
+
+
 def init_agent():
-	ai = agent.Network([10*10*3, 10*10*2])
+    ai = agent.Network([width * height * 3, width * height * 2])
+
 
 def play_move(ai, curr_grid):
-	grid_prob = ai.evaluate(curr_grid)
-	index = numpy.argmax(grid_prob)
-	position = index % (10*10)
+    grid_prob = ai.evaluate(curr_grid)
+    index = numpy.argmax(grid_prob)
 
-	# position, flag_or_not
-	return position / 10, position % 10, index / (10*10)
+    position, toggle_flag = divmod(index, width * height)
+    return minesweeper.Coords(*divmod(position, width)), toggle_flag
 
-	# cost = (TrueBoard - flagBoard) + 0.1*moves -> delta
+    # cost = (TrueBoard - flagBoard) + 0.1*moves -> delta
+
