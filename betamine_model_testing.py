8f7361be74755f269812e5b3fec391be22836e96
njhlai <nicholasjhlai@gmail.com>
2021-09-25 00:14:49 -0700

    Add more logging

--- betamine_model_testing.py
+++ betamine_model_testing.py
@@ -9,11 +9,20 @@ from Game_Generation import (
 from numpy import load
 import pickle
 import numpy as np
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import tensorflow as tf 
 import pandas as pd
 from collections import defaultdict
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+
+    except RuntimeError as e:
+        print(e)
 
 """ Next Time:
     Convolution layer to improve behavior with more complicated games?
@@ -67,11 +76,16 @@ def test_models(layers_struct, learning_rates, optimizers, losses, batches, epoc
                 for loss in losses:
                     for batch in batches:
                         for epoch in epochs:
+                            print(f"Creating model with layers {layers}")
                             model = create_model_list(layers)
+
+                            print(f"compiling with loss {loss}, optimizer {optimizer} and learning rate {learning_rate}")
                             compile_model(model, learning_rate, optimizer, loss())
+
+                            print(f"Fitting model with batch size {batch} in {epoch} epochs")
                             model.fit(training_states[0], training_states[1], batch_size = batch, epochs = epoch)
-                            success_rate =  test_model(model, deepcopy(evaluation_states),1)
-                            win_rate = test_against_game(model, 1000, game_options,1)
+                            success_rate =  test_model(model, deepcopy(evaluation_states), 1)
+                            win_rate = test_against_game(model, 1000, game_options, 1)
 
                             for i in range(len(layers)):
                                 layer_type, layer_attributes = layers[i]
@@ -80,7 +94,7 @@ def test_models(layers_struct, learning_rates, optimizers, losses, batches, epoc
                                     if layer_attributes[elem] == None:
                                         results[f"Layer{i+1}_{elem}"].append("None")
                                     elif callable(layer_attributes[elem]):
-                                         results[f"Layer{i+1}_{elem}"].append(layer_attributes[elem].__name__)
+                                        results[f"Layer{i+1}_{elem}"].append(layer_attributes[elem].__name__)
                                     else:
                                         results[f"Layer{i+1}_{elem}"].append(layer_attributes[elem])
                             results["LearningRate"].append(learning_rate)
@@ -94,32 +108,39 @@ def test_models(layers_struct, learning_rates, optimizers, losses, batches, epoc
 
 
 # Initialize the different parameters to test. Note, for layer structure, all of them must have the same number of layers and layer attributes (or else we can't construct the data frame because of different column size)
-test_parameters = {"layers_struct": [
-    [[tf.keras.layers.Dense, {"units": 20*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 10*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 5*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 1*n, "activation": None}]],
-    [[tf.keras.layers.Dense, {"units": 20*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 10*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 5*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 1*n, "activation": tf.keras.activations.sigmoid}]],
-    [[tf.keras.layers.Dense, {"units": 20*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 10*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 5*n, "activation":  tf.keras.activations.relu}],
-        [tf.keras.layers.Dense, {"units": 1*n, "activation": tf.keras.activations.softmax}]]
+test_parameters = {
+    "layers_struct": [
+        [
+            [tf.keras.layers.Dense, {"units": 20*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 10*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 5*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 1*n, "activation": None}]
         ],
-    "learning_rates": [1,0.1,0.01], 
+        [
+            [tf.keras.layers.Dense, {"units": 20*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 10*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 5*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 1*n, "activation": tf.keras.activations.sigmoid}]
+        ],
+        [
+            [tf.keras.layers.Dense, {"units": 20*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 10*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 5*n, "activation":  tf.keras.activations.relu}],
+            [tf.keras.layers.Dense, {"units": 1*n, "activation": tf.keras.activations.softmax}]
+        ]
+    ],
+    "learning_rates": [1, 0.1, 0.01],
     "optimizers": [tf.keras.optimizers.SGD], 
-    "losses": [tf.keras.losses.MeanSquaredError,tf.keras.losses.CategoricalCrossentropy], 
-    "batches": [30,100], 
-    "epochs": [3,5]
-    }
+    "losses": [tf.keras.losses.MeanSquaredError, tf.keras.losses.CategoricalCrossentropy],
+    "batches": [30, 100],
+    "epochs": [3, 5]
+}
 
 test_results = test_models(**test_parameters)
 
 # add the results to previously computed ones
 previous_result = pd.read_csv("Results/TrainingResults.csv")
-test_results = pd.concat([test_results,previous_result]).drop_duplicates().reset_index(drop=True)
+test_results = pd.concat([test_results, previous_result]).drop_duplicates().reset_index(drop=True)
 
 # save the results
 test_results.to_csv("Results/TrainingResults.csv", index=False)
